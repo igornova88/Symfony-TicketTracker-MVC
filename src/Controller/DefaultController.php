30d78a3ddf38a4a4841db0e13575243832b17cd3
igornova88 <igornova88@yahoo.com>
2020-05-07 23:08:25 +0200

    Connect to github repo

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,20 +1,29 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
     /**
      * @Route("/a")
     */
     public function number()
     {
+        // throw new \Exception('Something went wrong!');
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        $product = false;
+        // if (!$product) {
+            // throw $this->createNotFoundException('The product does not exist');
+    
+            // the above is just a shortcut for:
+            // throw new NotFoundHttpException('The product does not exist');
+        // }
+        return $this->render('base.html.twig', [
+            'number' => $number,
+        ]);
     }
 }
