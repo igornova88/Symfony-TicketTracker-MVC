8602ee1bc007ffc001afb9d4977a91a87881d412
igornova88 <igornova88@yahoo.com>
2020-05-14 23:40:21 +0200

    Add User entity and fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,12 +4,25 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use App\Entity\Project;
+use App\Entity\User;
 
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
+        
+        $this->loadUsers($manager);
+
+        // @todo - change this to loadProjects
         foreach ($this->getProjectData() as [$fullname, $active]) {
             $project = new Project();
             $project->setName($fullname);
@@ -18,8 +31,14 @@ class AppFixtures extends Fixture
         }
 
         $manager->flush();
-    }
 
+
+
+        $user = new User();
+
+        $manager->flush();
+
+    }
     
     private function getProjectData(): array
     {
@@ -28,6 +47,39 @@ class AppFixtures extends Fixture
             ['Project One', 1],
             ['Project Two', 1],
             ['Project Three', 0],
+        ];
+    }
+
+
+
+
+
+    private function loadUsers(ObjectManager $manager): void
+    {
+        foreach ($this->getUserData() as [$fullname, $username, $password, $email, $roles]) {
+            $user = new User();
+            // $user->setFullName($fullname);
+            // $user->setUsername($username);
+            $user->setPassword($this->passwordEncoder->encodePassword($user, $password));
+            $user->setEmail($email);
+            $user->setRoles($roles);
+
+            $manager->persist($user);
+            $this->addReference($username, $user);
+        }
+
+        $manager->flush();
+    }
+
+
+
+    private function getUserData(): array
+    {
+        return [
+            // $userData = [$fullname, $username, $password, $email, $roles];
+            ['Jane Doe', 'jane_admin', 'kitten', 'jane_admin@symfony.com', ['ROLE_ADMIN']],
+            ['Tom Doe', 'tom_admin', 'kitten', 'tom_admin@symfony.com', ['ROLE_ADMIN']],
+            ['John Doe', 'john_user', 'kitten', 'john_user@symfony.com', ['ROLE_USER']],
         ];
     }
 }
